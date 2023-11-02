34b5c5afb79ab046f691a0dfc950cd07feabe9da
edubernardi <77065000+edubernardi@users.noreply.github.com>
2023-11-01 22:27:40 -0300

    Implemented rook movement, fixed reconnection

--- server.py
+++ server.py
@@ -22,11 +22,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             playing = player2
             waiting = player1
         print(board.toPlay + " to play")
-        playing.sendall((board.unicodeBoard() + "\n Your move - " + board.toPlay + " to play").encode())
-        waiting.sendall((board.unicodeBoard() + "\n Waiting for opponent - " + board.toPlay + " to play").encode())
-        data = playing.recv(1024)
-        data = data.decode().split(' ')
-        print(data)
-        if len(data) == 2:
-            print('Jogada OK')
-            print('Jogada: ', board.play(data[0], data[1]))
+
+        try:
+            waiting.sendall((board.unicodeBoard() + "\n Waiting for opponent - " + board.toPlay + " to play").encode())
+        except WindowsError:
+            if board.toPlay == 'white':
+                player2, addr = s.accept()
+            else:
+                player1, addr = s.accept()
+
+        try:
+            playing.sendall((board.unicodeBoard() + "\n Your move - " + board.toPlay + " to play").encode())
+            data = playing.recv(1024)
+            data = data.decode().split(' ')
+            print(data)
+            if len(data) == 2:
+                print('Jogada OK')
+                print('Jogada: ', board.play(data[0], data[1]))
+        except WindowsError:
+            if board.toPlay == 'white':
+                player1, addr = s.accept()
+            else:
+                player2, addr = s.accept()
